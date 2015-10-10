71048d39b6ea18a575679867f5f16e4cf2a758d9
viorik <vpatrauc@gmail.com>
2015-10-10 21:28:41 +0100

    adding all files
    Signed-off-by: viorik <vpatrauc@gmail.com>

---
+++ src/caffe/test/cmake_test_defines.hpp.gen.cmake
@@ -0,0 +1,4 @@
+#define CUDA_TEST_DEVICE -1
+#define CMAKE_SOURCE_DIR "/scratch/DeconvNet/caffe/src/"
+#define EXAMPLES_SOURCE_DIR "/scratch/DeconvNet/caffe/examples/"
+#define CMAKE_EXT ".gen.cmake"
---
+++ src/caffe/test/test_data/sample_data_list.txt.gen.cmake
@@ -0,0 +1,2 @@
+/scratch/DeconvNet/caffe/src/caffe/test/test_data/sample_data.h5
+/scratch/DeconvNet/caffe/src/caffe/test/test_data/sample_data_2_gzip.h5
